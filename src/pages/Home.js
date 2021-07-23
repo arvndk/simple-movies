f8b2eef34627a5b20657db8afe93ff3557b6b528
arvn.dk <aravind@drawout.dk>
2021-07-23 13:01:02 +0200

    Added styled cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,52 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+//Redux
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+import Game from "../components/Game";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
+  //FETCH GAMES
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+  //Get that data back
+  const { popularGames, newGames, upcomingGames } = useSelector(
+    (state) => state.games
+  );
   return (
-    <div className="App">
-      <h1>Homepage</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcomingGames.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
